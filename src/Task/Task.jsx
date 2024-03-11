67e806bc98e171e06e0af465569e3002ac03d937
geetanjali <geethanjali.panuganti@gmail.com>
2024-03-11 14:30:44 +0530

    Your commit message here

---
+++ src/Task/Task.jsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+const Task = ({title,description,deleteTask,index}) => {
+
+    
+  return (
+    <div className = 'task'>
+        <div><p>{title}</p>
+      <span>{description}</span></div>
+      <button type='submit' onClick={() => deleteTask(index)} >X</button>
+    </div>
+   
+  )
+}
+
+export default Task
+
